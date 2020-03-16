322b24e7aa358c99b134ce6bd3c44087bca3e8e2
biomata95 <37086825+biomata95@users.noreply.github.com>
2020-03-16 15:19:34 -0300

    Update Inventario.cs

--- inventario/Inventario.cs
+++ inventario/Inventario.cs
@@ -12,9 +12,10 @@ class Inventario{
 	}
 
 	public void getProdutos(){ /* Método par imprimir os produtos */
+    Console.WriteLine("Produtos disponíveis: ");
     foreach (var produtos in  allProdutos)
     {
-      Console.WriteLine("Produto: {0},{1},{2}", produtos.id, produtos.nome, produtos.preco);
+      Console.WriteLine("Código: {0},Produto: {1},Preço:{2} R$", produtos.id, produtos.nome, produtos.preco);
     }
-	}
+  }
 }
