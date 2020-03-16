f317297aed3f0a96d32a07621c532f4ecb6c67ee
biomata95 <37086825+biomata95@users.noreply.github.com>
2020-03-16 17:18:23 -0300

    Create Quarto.cs

---
+++ hotel/Quarto.cs
@@ -0,0 +1,15 @@
+class Quarto{
+  public int numeroQuarto;
+  public double diaria;
+  public int suite;
+  public int ocupado;
+  public int codigoCliente;
+  public Quarto(int numeroQuarto,double diaria, int suite,int ocupado, int codigoCliente)
+	{
+    this.numeroQuarto = numeroQuarto;
+    this.diaria = diaria;
+		this.suite = suite;
+    this.ocupado = ocupado;
+    this.codigoCliente = codigoCliente;
+	}
+}
